043425515688e5ba193a75a7be28ec11e8e5d7d2
Zack Hall <zackhall@outlook.com>
2017-01-05 20:13:03 -0800

    Add v1 listener

--- app/components/ChatPane.js
+++ app/components/ChatPane.js
@@ -1,23 +1,48 @@
 import React, {Component} from 'react';
 import styles from './ChatPane.css';
+import irc from 'irc';
 
 export default class ChatPane extends Component {
+  constructor () {
+    super();
+    this.state = {
+      client: new irc.Client('chat.freenode.net', 'ack-chat', {
+        channels: ['#fun'],
+      }),
+      messages: [],
+    };
+  }
+
   render() {
+    const messages = this.state.messages.map(message => (
+      <li>
+        <div className={styles.user}>{message.from}</div>
+        <div className={styles.message}>{message.message}</div>
+      </li>
+    ));
+
     return (
       <div className={styles.chatPane}>
         <div className={styles.messages}>
           <ul>
-            <li>
-              <div className={styles.user}>zackhall</div>
-              <div className={styles.message}>Hello!</div>
-            </li>
-            <li>
-              <div className={styles.user}>svukel</div>
-              <div className={styles.message}>Welcome to Ack Chat!</div>
-            </li>
+            {messages}
           </ul>
         </div>
       </div>
     );
+  }
+
+  componentDidMount() {
+    this.state.client.addListener('message', (from, to, message) => {
+      this.setState({
+        messages: [
+          ...this.state.messages,
+          {
+            from: from,
+            message: message,
+          },
+        ]
+      });
+    });
   }
 }
